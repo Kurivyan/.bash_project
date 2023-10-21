7d582c1a085d28099dafb7575665d59f2d78e5fd
Kurivyan <arman.azamatov.2015@mail.ru>
2023-10-21 16:24:46 +0600

    1

--- main.c
+++ main.c
@@ -4,6 +4,40 @@
 #include<sys/types.h>
 #include<sys/wait.h>
 
+// Macro
+#define COMMAND_SIZE 30
+
+// Structures
+struct node_s1{
+
+};
+
+struct node_s2{
+
+};
+
+// Functions
+char* read_word();
+// -------------------------------------------------------------
+char* read_word(){
+	char* ptr;
+	char c;
+	c = getchar();
+	int i = 0;
+	ptr = malloc(sizeof(char) * COMMAND_SIZE);
+	while(c != '\n'){
+		ptr[i] = c;
+		i++;
+		c = getchar();
+	}
+	return ptr;
+}
+
 int main(){
+	char* raw_command;
+	do{
+		raw_command = read_word();
+		analyse_command(raw_commnd);
+	} while(1);
 	return 0;
 }
